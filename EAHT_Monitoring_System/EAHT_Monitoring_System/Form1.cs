c009108d047eb6a560a9da177c16cf14f7fe722d
AlmazAkylbekov <49546057+AlmazAkylbekov@users.noreply.github.com>
2019-12-01 19:38:21 +0000

    Add files via upload

--- EAHT_Monitoring_System/EAHT_Monitoring_System/Form1.cs
+++ EAHT_Monitoring_System/EAHT_Monitoring_System/Form1.cs
@@ -22,11 +22,18 @@ namespace EAHT_Monitoring_System
         {
 
         }
-
+        /// <summary>
+        /// Event for logging in the system when button clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
+            //Connection to the EAHT Database
             SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\EAHT_Database.mdf;Integrated Security=True;Connect Timeout=30;");
+            //Data adapter to the menegement staff database
             SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From staffDatabase where StaffID='" + Staff_ID_textbox.Text + "' and Password='" + Password.Text + "'", con);
+            //Data adapter to the medical staff database
             SqlDataAdapter mda = new SqlDataAdapter("Select Count(*) From medicsDatabase where MedicID='" + Staff_ID_textbox.Text + "' and Password='" + Password.Text + "'", con);
 
             DataTable dt2 = new DataTable();
@@ -34,25 +41,30 @@ namespace EAHT_Monitoring_System
             sda.Fill(dt);
             mda.Fill(dt2);
 
+            //Condition for checking if input username and password match with the ones in the Management Staff Database
             if (dt.Rows[0][0].ToString() == "1")
             {
+                //If user input is valid this window hides and new window for manager opens
                 this.Hide();
                 Form2 f2 = new Form2();
                 int idForStaff = Convert.ToInt32(Staff_ID_textbox.Text);
-                f2.updateStaffDetails(idForStaff);
+                f2.updateStaffDetails(idForStaff); //Updating menegement staff details
                 f2.ShowDialog();
             }
 
+            //Conditions for checking input username and password match with the ones in the Medical Team Database
             else if (dt2.Rows[0][0].ToString()=="1")
             {
-                RegisterShift();
+                //If user input is valid, new window for medical team member opens
+                RegisterShift();    //Registration of shift's startting time
                 this.Hide();
                 Form3 f3 = new Form3();
                 int idForMedics = Convert.ToInt32(Staff_ID_textbox.Text);
-                f3.updateMedicsDetails(idForMedics);
+                f3.updateMedicsDetails(idForMedics);  //Updating medics details
                 f3.ShowDialog();
             }
 
+            //Else condition for displaying error message if user inputs are not valid
             else
             {
                 MessageBox.Show("Please check your StaffID and password");
